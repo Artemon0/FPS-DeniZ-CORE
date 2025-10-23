078a27a29caa7c851da3f88f7f186e918ae9e38a
Artem Onyshchenko <artemon888.com@gmail.com>
2025-10-23 13:30:15 +0300

    Updated script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class MyCameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+
+    float rotationX = 0f;
+    float rotationY = 0f;
+
     void Start()
     {
-        
+        // Захватываем и скрываем курсор
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Движение по клавишам
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+        // Вращение мышью
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationY += mouseX;
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f); // Ограничение по вертикали
+
+        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
     }
 }
