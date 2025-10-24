65e9caa99852e22bc6d5b1296aca2b1c30e7cb70
Artem Onyshchenko <artemon888.com@gmail.com>
2025-10-24 09:17:29 +0300

    Removed lock cursor

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -16,8 +16,8 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        // Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.visible = false;
     }
 
     void Update()
